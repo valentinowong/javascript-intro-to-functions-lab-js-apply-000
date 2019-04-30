dcf9e07ec4aeecde2c67266ac37d6e64452d2972
Valentino Wong <valentino.wong@gmail.com>
2019-04-30 20:29:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,6 +18,8 @@ function sayHiToGrandma(string) {
   if (string === "I love you, Grandma.") {
     return "I love you, too."
   } else if (string === string.toLowerCase()) {
-    return 
+    return "I can't hear you!"
+  } else if (string === string.toUpperCase()) {
+    
   }
 }
